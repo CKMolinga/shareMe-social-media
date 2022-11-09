4c3e39ef64bd4ab892c75dd84090803d91ab8a87
CKMolinga <ck.molinga@gmail.com>
2022-11-10 00:45:23 +0100

    Google client credentials

--- shareme-frontend/src/components/Login.jsx
+++ shareme-frontend/src/components/Login.jsx
@@ -10,9 +10,15 @@ import shareVideo from "../assets/share.mp4";
 import logo from "../assets/logowhite.png";
 
 const Login = () => {
+  // Functions
+  const responseGoogle = (response) => {
+    console.log(response);
+  };
+
   return (
     <div className=" flex justify-start items-center flex-col h-screen">
       <div className="relative w-full h-full">
+        {/* Background Video */}
         <video
           src={shareVideo}
           type="video/mp4"
@@ -22,6 +28,31 @@ const Login = () => {
           autoPlay
           className="w-full h-full object-cover"
         />
+
+        {/* Overlay */}
+        <div className="absolute flex flex-col justify-center items-center top-0 right-0 left-0 bottom-0 bg-blackOverlay">
+          <div className="p-5">
+            <img src={logo} alt="logo" className="w-40" />
+          </div>
+          <div className="shadow-2xl">
+            <GoogleLogin
+              clientId={process.env.REACT_APP_GOOGLE_CLIENT_ID}
+              render={(renderProps) => (
+                <button
+                  type="button"
+                  className="bg-mainColor flex justify-center items-center p-3 rounded-lg cursor-pointer outline-none"
+                  onClick={renderProps.onClick}
+                  disabled={renderProps.disabled}
+                >
+                  <FcGoogle className="mr-4" /> Sign in with Google
+                </button>
+              )}
+              onSuccess={responseGoogle}
+              onFailure={responseGoogle}
+              cookiePolicy={"single_host_origin"}
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
